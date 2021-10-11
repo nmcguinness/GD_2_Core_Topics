042ffc243211ed9d9a95e3099aaca2c6a0f31d37
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-11 12:32:23 +0100

    Added implementation of SoundSelectionResponse

--- GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/SoundSelectionResponse.cs
+++ GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/SoundSelectionResponse.cs
@@ -4,14 +4,27 @@ namespace GD.Selection
 {
     public class SoundSelectionResponse : MonoBehaviour, ISelectionResponse
     {
+        [SerializeField]
+        private AudioClip selectedAudioClip;
+
+        private Transform currentSelectedTransform = null;
+
         public void OnDeselect(Transform transform)
         {
-            throw new System.NotImplementedException();
+            //no sound on deselect
         }
 
         public void OnSelect(Transform transform)
         {
-            throw new System.NotImplementedException();
+            if (currentSelectedTransform != null
+                && currentSelectedTransform != transform)
+            {
+                AudioSource.PlayClipAtPoint(selectedAudioClip,
+                    transform.position);
+            }
+
+            //store what we newly pointed at
+            currentSelectedTransform = transform;
         }
     }
 }
