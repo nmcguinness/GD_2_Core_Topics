2e248f8f5a51f10c57e7498783558adc2292d5d5
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-14 12:10:14 +0100

    Added TargetSelection

--- GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/TargetSelectionResponse.cs
+++ GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/TargetSelectionResponse.cs
@@ -4,14 +4,46 @@ namespace GD.Selection
 {
     public class TargetSelectionResponse : MonoBehaviour, ISelectionResponse
     {
-        public void OnDeselect(Transform transform)
+        [SerializeField]
+        [Tooltip("Set prefab object used for target highlighting")]
+        private GameObject targetSelectionPrefab;
+
+        [SerializeField]
+        private LayerMask targetGroundLayerMask;
+
+        private GameObject currentTargetInstance;
+
+        [SerializeField]
+        [Tooltip("Vertical offset on target highlight above ground layer")]
+        private float targetOffset;
+
+        private float scaleFactor = 5;
+        private int rayCastDepth = 10;
+
+        public void Awake()
         {
-            throw new System.NotImplementedException();
+            currentTargetInstance = Instantiate(targetSelectionPrefab, Vector3.zero, Quaternion.Euler(0, 0, 0));
+            currentTargetInstance.SetActive(false);
         }
 
-        public void OnSelect(Transform transform)
+        public void OnSelect(Transform selection)
         {
-            throw new System.NotImplementedException();
+            if (currentTargetInstance != null)
+            {
+                RaycastHit hitInfo;
+                if (Physics.Raycast(selection.position, -selection.up, out hitInfo, rayCastDepth, targetGroundLayerMask))
+                {
+                    currentTargetInstance.transform.position = selection.position - new Vector3(0, hitInfo.distance - targetOffset, 0);
+                    float mag = selection.GetComponent<Collider>().bounds.size.magnitude / scaleFactor;
+                    currentTargetInstance.transform.localScale = new Vector3(mag, mag, mag);
+                    currentTargetInstance.SetActive(true);
+                }
+            }
+        }
+
+        public void OnDeselect(Transform selection)
+        {
+            currentTargetInstance.SetActive(false);
         }
     }
 }
