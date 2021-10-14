2dce463479da04d08c737603944f32b6a682c872
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-14 13:01:21 +0100

    Refactored target selection response

--- GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/TargetSelectionResponse.cs
+++ GD_2_Core_Topics/Assets/GD/Common/Scripts/Selection/Reponses/TargetSelectionResponse.cs
@@ -11,18 +11,26 @@ namespace GD.Selection
         [SerializeField]
         private LayerMask targetGroundLayerMask;
 
-        private GameObject currentTargetInstance;
-
         [SerializeField]
         [Tooltip("Vertical offset on target highlight above ground layer")]
-        private float targetOffset;
+        [Range(0.01f, 10)]
+        private float targetOffset = 0.01f;
 
+        [SerializeField]
+        [Tooltip("Sets scale factor on object dimension dependent target prefab")]
+        [Range(1, 10)]
         private float scaleFactor = 5;
+
+        private GameObject currentTargetInstance;
+
+        //TODO - Make serialise field?
         private int rayCastDepth = 10;
 
         public void Awake()
         {
-            currentTargetInstance = Instantiate(targetSelectionPrefab, Vector3.zero, Quaternion.Euler(0, 0, 0));
+            currentTargetInstance = Instantiate(targetSelectionPrefab,
+                Vector3.zero, Quaternion.Euler(0, 0, 0));
+
             currentTargetInstance.SetActive(false);
         }
 
@@ -31,7 +39,8 @@ namespace GD.Selection
             if (currentTargetInstance != null)
             {
                 RaycastHit hitInfo;
-                if (Physics.Raycast(selection.position, -selection.up, out hitInfo, rayCastDepth, targetGroundLayerMask))
+                if (Physics.Raycast(selection.position, -selection.up,
+                    out hitInfo, rayCastDepth, targetGroundLayerMask))
                 {
                     currentTargetInstance.transform.position = selection.position - new Vector3(0, hitInfo.distance - targetOffset, 0);
                     float mag = selection.GetComponent<Collider>().bounds.size.magnitude / scaleFactor;
