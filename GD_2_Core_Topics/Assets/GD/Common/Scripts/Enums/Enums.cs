3ea73a44c8418a18877b76ebc68a879c0e95ed49
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-21 13:36:29 +0100

    Added new SO types

--- GD_2_Core_Topics/Assets/GD/Common/Scripts/Enums/Enums.cs
+++ GD_2_Core_Topics/Assets/GD/Common/Scripts/Enums/Enums.cs
@@ -8,4 +8,41 @@
     {
         On, Off
     }
+
+    /// <summary>
+    /// Used in a ScriptableObject to indicate the type of the item about which the SO is storing information
+    /// </summary>
+    /// <see cref="Objects.BaseObjectData"/>
+    public enum ItemType : sbyte
+    {
+        Building, Equipment, Food, Medicine, NPC, Player, Story, Weapon
+    }
+
+    /// <summary>
+    /// Defines the core attributes that an object can modify in a player.
+    /// For example, a health pickup will modify speed, strength, or stamina.
+    /// </summary>
+    /// <see cref="Objects.ConsumableObjectData"/>
+    public enum AttributeType : sbyte
+    {
+        Agility, Intelligence, Knowledge, Mana, Sight, Speed, Strength, Stamina
+    }
+
+    /// <summary>
+    /// Defines the attack type of a NPC
+    /// </summary>
+    /// <see cref="ARVR.Objects.PlaceableObjectData"/>
+    public enum AttackType : sbyte
+    {
+        Melee, Ranged
+    }
+
+    /// <summary>
+    /// Defines the type of unit that an NPC will attack
+    /// </summary>
+    /// <see cref="ARVR.Objects.PlaceableObjectData"/>
+    public enum AttackTargetType : sbyte
+    {
+        Any, Building, None, Unit, Vehicle
+    }
 }
