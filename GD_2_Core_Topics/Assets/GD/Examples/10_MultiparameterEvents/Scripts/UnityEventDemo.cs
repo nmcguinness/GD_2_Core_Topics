2ba6e3c5339c353bfcd5f5c0a8da645fd3956be4
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-22 20:27:13 +0000

    Fixed shader with Matrix bug and renamed some assets and shader variables for clarity

--- GD_2_Core_Topics/Assets/GD/Examples/10_MultiparameterEvents/Scripts/UnityEventDemo.cs
+++ GD_2_Core_Topics/Assets/GD/Examples/10_MultiparameterEvents/Scripts/UnityEventDemo.cs
@@ -1,6 +1,52 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
+public class PickupEventArgs : EventArgs
+{
+    public int value;
+    public GameObject pickedUpObject;
+}
+
+public class GDUnityEvent : UnityEvent<EventArgs>
+{
+}
+
+public class CustomUnityEvent : UnityEvent<string>
+{
+}
+
+public class UnityEventDemo : MonoBehaviour
+{
+    private GDUnityEvent someEvent;
+    private float timeSinceLastEventSecs;
+
+    private void Start()
+    {
+        if (someEvent == null)
+            someEvent = new GDUnityEvent();
+
+        someEvent.AddListener(SomeFunction);
+    }
+
+    private void Update()
+    {
+        timeSinceLastEventSecs += Time.deltaTime;
+
+        if (timeSinceLastEventSecs > 2)
+        {
+            timeSinceLastEventSecs -= 2;
+            someEvent.Invoke(new PickupEventArgs());
+        }
+    }
+
+    private void SomeFunction(EventArgs gameEventArgs)
+    {
+        Debug.Log(gameEventArgs);
+    }
+}
+
+/*
 public class UnityEventDemo : MonoBehaviour
 {
     private UnityEvent someEvent;
@@ -30,3 +76,4 @@ public class UnityEventDemo : MonoBehaviour
         Debug.Log("Something happened...");
     }
 }
+*/
