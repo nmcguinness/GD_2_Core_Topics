c499cf0d1f4447b94b9863727a021340320db816
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-29 08:26:09 +0000

    Adding multi parameter event support

---
+++ GD_2_Core_Topics/Assets/GD/Common/Scripts/Patterns/Events/MultiParameter/Pickup/PickupEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GD.Events
+{
+    public enum PickupType : sbyte
+    {
+        Health, Ammo, Money, Mana
+    }
+
+    public struct PickupData
+    {
+        private int value;
+        private PickupType type;
+    }
+
+    [CreateAssetMenu(fileName = "PickupEvent", menuName = "Scriptable Objects/Events/Pickup")]
+    public class PickupEvent : BaseGameEvent<PickupData>
+    {
+        public override void Raise(PickupData parameters)
+        {
+            base.Raise(parameters);
+        }
+    }
+
+    [Serializable]
+    public class UnityPickupEvent : UnityEvent<PickupData>
+    {
+    }
+
+    public class PickupGameEventListener : BaseGameEventListener<PickupData, PickupEvent, UnityPickupEvent>
+    {
+    }
+}
